dbc4a75038031183c475a9bd0fa17a1156d98805
kyandro-casti <kyandro2004@gmail.com>
2024-07-18 05:38:49 +0200

    first commit

---
+++ hello world/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int one = 10;
+        int two = 5;
+        
+        int Result1 = one + two;
+
+
+        int result2 = one / two;
+        Console.WriteLine(one + " + " + two + " = " + Result1 + " and " + one + " divided by " + two + " is equal to " + result2);
+        Console.WriteLine("also hello to the world");
+    }
+}
